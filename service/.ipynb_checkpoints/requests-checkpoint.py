5c2656f3302afcc4a54403572a46a37bd41b3e5f
İrfan Baykara <63783207+irfanbykara@users.noreply.github.com>
2026-01-08 19:03:48 +0300

    Delete service/.ipynb_checkpoints directory

--- service/.ipynb_checkpoints/requests-checkpoint.py
+++
@@ -1,9 +0,0 @@
-"""Request objects"""
-from pydantic import BaseModel, Field
-
-
-class Sam2EncoderRequest(BaseModel):
-    """Request model for Sam2Encoder."""
-    image_base64: str = Field(..., description="Base64 encoded image to be encoded.")
-
-
